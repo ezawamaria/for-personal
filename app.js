eaf3fee3d28017ed488a38a97824e280f9e48e87
ezawamaria <126713296+ezawamaria@users.noreply.github.com>
2025-02-14 16:35:10 +0800

    Update app.js

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.get("/list", function (req, res) {
            .filter(Boolean);
   
         if (processes.length === 0) {
-            return res.json({ status: "error", message: "无进程运行"});
+            return res.json({ status: "error", processes: "无进程运行"});
         }
         // 返回美化格式的JSON
         res.type('json').send(JSON.stringify({ status: "success", processes }, null, 2));
